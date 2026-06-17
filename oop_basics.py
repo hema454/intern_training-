fb20a73f54f66d51d56fea496c57038389f30b50
Hemavarshini <hemavarshini343@gmail.com>
2026-06-17 10:18:49 +0530

    Added oop basics

--- oop_basics.py
+++ oop_basics.py
@@ -34,35 +34,43 @@ if choice==2:
         self.Ifsc=Ifsc
         self.balance=balance
      def deposit(self):
-        account_no=int(input('enter account no:'))
-        Ifsc_code=input('enter ifsc:')
-        if self.account_no==account_no and self.Ifsc==Ifsc_code:
             amount=int(input("enter deposit amount"))
             self.balance+=amount
             print("amount deposited successfully")
-        else:
-           print("invalid account number")
-
      def withdraw(self):
-        account_no=int(input('enter account no:'))
-        Ifsc_code=input('enter ifsc:')
-        if self.account_no==account_no and self.Ifsc==Ifsc_code:
              amount=int(input("enter withdrawal amount"))
              if amount<=self.balance:
               self.balance-=amount
               print("amount withdrawn successfully")
              else:
-                print("insufficient balance")
-        else:
-             print("invalid account number")
+              print("insufficient balance")
 
      def check_balance(self):
        print("Available balance",self.balance)
+     def end(self):
+        print("🙏🙏Thank you visit again🙏🙏")
     cst1=bank("Hema",21,12345678,"ABCNO",40000)
     cst2=bank("sreya",25,90223154,"DEF",20000)
-    cst1.deposit()
-    cst1.withdraw()
-    cst1.check_balance()
-    cst2.deposit()
-    cst2.withdraw()
-    cst2.check_balance()
+
+acc = int(input("Enter account number: "))
+     
+if acc == cst1.account_no:
+    customer = cst1
+elif acc == cst2.account_no:
+    customer = cst2
+else:
+    print("Invalid account number")
+while True:
+       print("1.deposit")
+       print("2.withdraw")
+       print("3.check balance")
+       print("4.exit")
+       choice=int(input("enter your choice"))
+       if choice==1:
+          customer.deposit()
+       if choice==2:
+          customer.withdraw()
+       if choice==3:
+          customer.check_balance()
+       if choice==4:
+          customer.end()
